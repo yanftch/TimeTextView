b26111124e44df02daeae94ec50563e02f4e6a49
IvenMax <791689609@qq.com>
2016-09-08 15:32:59 +0800

    Update MainActivity.java

--- app/src/main/java/com/iven/timetextview/MainActivity.java
+++ app/src/main/java/com/iven/timetextview/MainActivity.java
@@ -1,17 +1,22 @@
 package com.iven.timetextview;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.iven.timetextview.view.TimeTextView;
 
 public class MainActivity extends AppCompatActivity {
     private TimeTextView tv_show;
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
+        textView = (TextView) findViewById(R.id.textview);
     }
 
     private void initViews() {
@@ -21,7 +26,15 @@ public class MainActivity extends AppCompatActivity {
         tv_show.setIsStyleable(true);//设置倒计时可以显示多种颜色支持
         tv_show.setHeadInfo("你的时间不多了");//设置倒计时的提示头信息(内部自带一个英文的冒号)
         tv_show.setEndInfo("王炸！！！");//设置倒计时结束后的提示信息
-        tv_show.setLeftTime(10000);//设置倒计时的时间
+
+
+        tv_show.setLeftTime(10000, true, new TimeTextView.onCountDownListener() {
+            @Override
+            public void onTimeOverListener() {
+                textView.setText("倒计时结束了");
+                textView.setTextColor(Color.RED);
+            }
+        });
 
     }
 }
